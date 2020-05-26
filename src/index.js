a706d4c27a8e182f50a3700088b34b1183e8ce94
Edgar <edgar.a.rivera08@gmail.com>
2020-05-26 19:57:05 -0400

    Created welcome div to greet the player

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const guessWhoImage = document.querySelector('#guess-who-image')
   const body = document.querySelector('body')
   const personDiv = document.querySelector("#game-board")
-  const newPlayerForm = document.createElement('form')
-  newPlayerForm.id = "new-player-form"
   const peopleURL = 'http://localhost:3000/api/v1/people'
   const playerURL = 'http://localhost:3000/api/v1/players'
-  const backButton = document.createElement('button')
-  backButton.innerText = "Back"
+  // const backButton = document.createElement('button')
+  // backButton.innerText = "Back"
+  const welcomeDiv = document.querySelector('#welcome-message')
   
+  
+  const newPlayerForm = document.createElement('form')
+  newPlayerForm.id = "new-player-form"
   newPlayerForm.innerHTML = `<label for="name">Your name:</label>
   <input  type="text" id="name" name="name"><br>
   <input id= "submit-button" type="submit" value="Submit"></input>`
@@ -23,43 +25,52 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(newPlayerForm.name.value)
     fetch("http://localhost:3000/api/v1/players",{
       method: "POST",
-      headers: {"content-type":"application/json",
-    "accept":"application/json"},
+      headers: {
+        "content-type":"application/json",
+        "accept":"application/json"},
       body: JSON.stringify({
         name: newPlayerForm.name.value
       })
     })
     .then(res => res.json())
+    .then(renderPlayer)
 
-        .then(data => console.log(data))
-        .then(newPlayerForm.remove)
-      .then(data => createCharacterBoard(data))
-      .then(newPlayerForm.remove())
+    fetch(peopleURL)
+    .then(res => res.json())
+    .then(data => createCharacterBoard(data))
 
+    newPlayerForm.remove()
   })
+
+  function renderPlayer(player) {
+    const playerHeader = document.createElement('h1')
+    playerHeader.innerText = `Welcome ${player.name}`
+    console.log(playerHeader)
+    welcomeDiv.append(playerHeader)
+  }
   
   document.addEventListener('click', e =>{
     if(e.target.className === 'play-btn'){
+      const playBtn = e.target.parentNode
 
       guessWhoImage.remove()
       body.appendChild(newPlayerForm)
-      body.appendChild(backButton)
+      playBtn.remove()
 
-      // The 3 lines below belong within the submit function above
+      // The 3 lines below belong within the submit listener above
       // here for test purposes 
 
-      fetch(peopleURL)
-
-      .then(res => res.json())
-      .then(data => createCharacterBoard(data))
+      // fetch(peopleURL)
+      // .then(res => res.json())
+      // .then(data => createCharacterBoard(data))
 
       // newPlayerForm.remove()
       e.target.parentNode.remove()
+
     } else if(e.target.className === 'flip-btn'){
       const button = e.target
       const buttonParentNode = button.parentNode
       buttonParentNode.remove()
-      // console.log(buttonParentNode)
     }
     
     
