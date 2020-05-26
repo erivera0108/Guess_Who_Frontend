dff623a8fae271884fb0d5fe578e3adc6d2ad2f1
William <willlin1996@gmail.com>
2020-05-26 10:09:19 -0400

    newPlayer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   const guessWhoImage = document.querySelector('#guess-who-image')
+  const newPlayerForm = document.createElement('form')
+  newPlayerForm.innerHTML = `<label for="lname">Your name:</label>
+  <input id= "" type="text" id="name" name="name"><br><br>
+  <input id= "submit-button" type="submit" value="Submit"></input>`
 
+  const backButton = document.createElement('button')
+  backButton.innerText = "Back"
+  newPlayerForm.id = "new-player-form"
+
+  const body = document.querySelector('body')
+
+  document.addEventListener("submit", function(e){
+    if(e.target.className === '#submit-button'){
+      // POST
+    }
+  })
+  
   document.addEventListener('click', e =>{
     if(e.target.className === 'play-btn'){
       guessWhoImage.remove()
+      body.appendChild(newPlayerForm)
+      body.appendChild(backButton)
     }
+    
 
   })
 
