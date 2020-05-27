e79c4bc1e3f20ccbb72b38bfa2c7fbb390467736
William <willlin1996@gmail.com>
2020-05-27 12:02:06 -0400

    edit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const guessWhoImage = document.querySelector('#guess-who-image')
   const body = document.querySelector('body')
   const personDiv = document.querySelector("#game-board")
   const peopleURL = 'http://localhost:3000/api/v1/people'
   const playerURL = 'http://localhost:3000/api/v1/players'
-  // const backButton = document.createElement('button')
-  // backButton.innerText = "Back"
+  const backButton = document.createElement('button')
+  backButton.innerHTML = `<a href="">Back</a>`
   const welcomeDiv = document.querySelector('#welcome-message')
+
+
+  const delPersonForm = document.createElement('form')
+  delPersonForm.id = "del-player-form"
+  delPersonForm.innerHTML = `<label for="name">Person's:</label>
+  <input  type="text" id="del-name" name="name"><br>
+  <input id= "submit-button" type="submit" value="Delete"></input>`
   
   
   const newPlayerForm = document.createElement('form')
@@ -15,22 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
   newPlayerForm.innerHTML = `<label for="name">Your name:</label>
   <input  type="text" id="name" name="name"><br>
   <input id= "submit-button" type="submit" value="Submit"></input>`
-  const editNav = document.getElementById("edit-nav")
+
 
   const newPersonForm = document.createElement('form')
-  newPersonForm.innerHTML =   `<label for="name">Character Name:</label>
-  <input  type="text" id="char" name="name"><br>
-  <label for="name">Character Image:</label>
-  <input  type="text" id="pic" name="name"><br></br>
+  newPersonForm.id = "new-person-form"
+  newPersonForm.innerHTML = `<label for="name">Person's name:</label>
+  <input  type="text" id="char-name" name="name"><br>
+  <label for="name">Person's Pic</label>
+  <input  type="text" id="char-pic" name="pic"><br>
   <input id= "char-submit-button" type="submit" value="Submit"></input>`
-  const welcomeMessage = document.querySelector('.welcome-message')
+  
 
 
   
-/** 
+
   document.addEventListener("submit", function(e){
-    if (e.target.id === "submit-button"){
     e.preventDefault()
+    if(e.target === newPlayerForm){
     console.log(newPlayerForm.name.value)
     fetch(playerURL,{
       method: "POST",
@@ -42,39 +49,41 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     })
     .then(res => res.json())
-    .then(data => console.log(data))
-        
-    // .then(data => createCharacterBoard(data))
-    // .then(newPlayerForm.remove())
-  } else if(e.target.id === "char-submit-button"){
-    e.preventDefault()
-    console.log(newPersonForm.name.value)
-    console.log(newPersonForm.pic.value)
-    fetch(peopleURL,{
+    .then(renderPlayer)
+console.log('hey')
+    fetch(peopleURL)
+    .then(res => res.json())
+    .then(data => createCharacterBoard(data))
+
+    newPlayerForm.remove()
+    alert("You may not make changes to the gameboard once match starts!")
+  } else if (e.target === newPersonForm){
+    console.log(newPersonForm.name.value, newPersonForm.pic.value)
+  
+    fetch(peopleURL, {
       method: "POST",
-      headers: {"content-type":"application/json",
-    "accept":"application/json"},
+      headers: {
+        "content-type":"application/json",
+        "accept":"application/json"},
       body: JSON.stringify({
         name: newPersonForm.name.value,
         picture: newPersonForm.pic.value
-      })
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
-    .then(renderPlayer)
-    //This where the first GET request should take place Don't delete
-      //fetch(peopleURL)
-      //.then(res => res.json())
-      //.then(data => createCharacterBoard(data))
-      
-
-    newPlayerForm.remove()
   })
-*/
+  .then(res => res.json())
+  .then(console.log)
+
+  fetch(peopleURL)
+  .then(res => res.json())
+  .then(data => createCharacterBoard(data))
+  newPersonForm.remove()
+  backButton.remove()
+  }
+  })
 
   function renderPlayer(player) {
     const playerHeader = document.createElement('h1')
+    playerHeader.id = "welcome-header"
     playerHeader.innerText = `Welcome ${player.name}`
     console.log(playerHeader)
     welcomeDiv.append(playerHeader)
@@ -86,14 +95,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
       guessWhoImage.remove()
       body.appendChild(newPlayerForm)
+      body.appendChild(backButton)
       playBtn.remove()
+      
 
       // The 3 lines below belong within the submit listener above
       // here for test purposes 
 
-      fetch(peopleURL)
-      .then(res => res.json())
-      .then(data => createCharacterBoard(data))
+      // fetch(peopleURL)
+      // .then(res => res.json())
+      // .then(data => createCharacterBoard(data))
 
       // newPlayerForm.remove()
       e.target.parentNode.remove()
@@ -105,18 +116,28 @@ document.addEventListener("DOMContentLoaded", () => {
       const image = buttonParentNode.children[1]
         if (image.style.filter === '') {
           image.style.filter = 'grayscale(100%)'
-          // image.style.visibility = "hidden"
+          // image.style.visibility = “hidden”
           name.style.visibility = 'hidden'
         } else {
           image.style.filter = ''
-          // image.style.visibility = ''
+          // image.style.visibility = ‘’
           name.style.visibility = ''
         }
-        
-    } else if(e.target.id === "edit-nav"){
+    } else if (e.target.id === 'edit'){
+      newPlayerForm.remove()
+      const welcomeHeader = document.querySelector('#welcome-header')
+      const playBtn = document.getElementById('select-version')
+      if (!welcomeHeader){
+        playBtn.remove()
+      }
+      guessWhoImage.remove()
       body.appendChild(newPersonForm)
       body.appendChild(backButton)
-      newPersonForm.reset()
+      
+      if (welcomeHeader){
+        alert("No cheating!")
+        newPersonForm.remove()
+      }
     }
     
     
@@ -137,7 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     div.className = "card"
     div.dataset.id = person.id
     div.innerHTML = `
-    <h2 >${person.name}</h2>
+    <h2>${person.name}</h2>
     <img src=${person.picture} class="person-avatar"/>
     <button class="flip-btn"> Flip Card </button>
     `
